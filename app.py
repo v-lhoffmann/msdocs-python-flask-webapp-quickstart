d9791660d853cdbbd630f8a88b1f0bc56ae3f0c1
v-lhoffmann <99043209+v-lhoffmann@users.noreply.github.com>
2024-03-06 16:38:55 -0800

    Update app.py
    Trying again with formatting

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import random
-import cProfile, pstats, io
+import cProfile
+import pstats
+import io
 from pstats import SortKey
 
 from flask import (Flask, redirect, render_template, request,
@@ -8,22 +10,24 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
+
 def square():
-  i = 1
-  ans = 0
-  while i < 1000:
-    ans = i * i
+    i = 1
+    ans = 0
+    while i < 1000:
+        ans = i * i
+
 
 def bigmem():
-  arr = []
-  while i < 100:
-    arr.append(random.randint(1,10000))
+    arr = []
+    while i < 100:
+        arr.append(random.randint(1, 10000))
+
 
 @app.route('/')
-  def index():
+def index():
     print('Request for index page received')
     return render_template('index.html')
-
 
 
 @app.route('/favicon.ico')
@@ -31,30 +35,31 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    name = request.form.get('name')
 
-   if name:
-       if name == "square":
-         pr = cProfile.Profile()
-         pr.enable()
-         square()
-         pr.disable()
-         s = io.StringIO()
-         sortby = SortKey.CUMULATIVE
-         ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-         ps.dump_stats('output.txt')
-         ps.print_stats()
-         print(s.getvalue())
-       elif name == "bigmem":
-         bigmem()
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    if name:
+        if name == "square":
+            pr = cProfile.Profile()
+            pr.enable()
+            square()
+            pr.disable()
+            s = io.StringIO()
+            sortby = SortKey.CUMULATIVE
+            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            ps.dump_stats('output.txt')
+            ps.print_stats()
+            print(s.getvalue())
+        elif name == "bigmem":
+            bigmem()
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name=name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
